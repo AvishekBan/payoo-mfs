9d92022bb14108ae3fd394faf48437df1726efbe
Avishek Banerjee <banerjeeavishek2018@gmail.com>
2024-09-21 01:56:37 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,10 @@
+// console.log("i am hungry")
+// step-1 set event handler
+document.getElementById("button-login").addEventListener("click", function(event){
+    //step-2 prevent default behavior(prevent browser loading) 
+    event.preventDefault(); 
+    console.log('login button click');
+    // step-3 get the phone number
+const phoneNumber = document.getElementById('phone-number').value;
+console.log(phoneNumber);
+})
