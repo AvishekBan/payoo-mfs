24a2ab0384b7a486bba5eb685420519c3f1fdd02
Avishek Banerjee <banerjeeavishek2018@gmail.com>
2024-09-21 03:50:18 +0600

    second commit

--- js/login.js
+++ js/login.js
@@ -3,8 +3,21 @@
 document.getElementById("button-login").addEventListener("click", function(event){
     //step-2 prevent default behavior(prevent browser loading) 
     event.preventDefault(); 
-    console.log('login button click');
+    // console.log('login button click');
     // step-3 get the phone number
 const phoneNumber = document.getElementById('phone-number').value;
-console.log(phoneNumber);
+
+    // step-3 get the pin 
+const pinNumber = document.getElementById('pin-code').value;
+console.log(phoneNumber, pinNumber);
+
+//step-4 validate phone and pin
+// this is temporary
+if(phoneNumber === "01" && pinNumber === '1234'){
+    console.log('yon are logged in');
+    window.location.href = './home.html';
+}
+else{
+    alert('wrong phone number or pin');
+}
 })
