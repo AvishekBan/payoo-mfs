4c71588c61bda054fb376f0c4caa27c49ad44ede
Avishek Banerjee <banerjeeavishek2018@gmail.com>
2024-09-22 10:44:21 +0600

    i am commited

--- js/home.js
+++ js/home.js
@@ -1,1 +1,15 @@
-console.log('home added')
+// add money to the account
+
+// step-1 add an event handler to the add money button
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    // prevent auto reload 
+   event.preventDefault();
+   
+    // step-2 get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    
+    // get the pin number provided
+
+    const addPinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(addMoneyInput,addPinNumberInput);
+});
